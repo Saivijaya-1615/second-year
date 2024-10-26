45e06d26d44aaa20cc05c878c23a1397385b3fc1
Elli Vijaya Lakshmi <vijayalakshmi76330@gmail.com>
2024-10-26 23:16:35 +0530

    Update evenoroddchecker.py

--- evenoroddchecker.py
+++ evenoroddchecker.py
@@ -1,13 +1,11 @@
 def check_even_odd(num):
     try:
-        # Check if the number is even
         if num % 2 == 0:
             print(f"{num} is even")
         # If not even, then it's odd
         else:
             print(f"{num} is odd")
     except TypeError:
-        # Handle the exception if input is not an integer
         print("Please enter a valid integer.")
 check_even_odd(7) 
 check_even_odd(-4)  
