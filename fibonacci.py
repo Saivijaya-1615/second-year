68d610fa3e9f44513bc06f6eddc11fbf71f510ea
Elli Vijaya Lakshmi <vijayalakshmi76330@gmail.com>
2024-10-27 21:06:50 +0530

    Update fibonacci.py
    If n is negative then gives error message.

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,7 @@
 def fibonacci(n):
-    if n <= 1:
+    if not isinstance(n, int) or n < 0:
+        raise ValueError("Input must be a non-negative integer")
+    if n <= 1 :
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
